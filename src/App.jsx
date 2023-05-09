75156ba3c91d219eae6ff88817d86bca3003d196
msandypr <msandypr@gmail.com>
2023-05-09 09:29:05 +0700

    Set Up React-Webcam

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Webcam from "react-webcam"
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const videoOption = {
+    width: 720,
+    height: 480,
+    facingMode: "environment"
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return(
+    <div className="App">
+      <h1>Simple Object Detection</h1>
+      <p>By <a href="https://msandypr.com">@msandypr</a></p>
+      <Webcam
+        // ref={webcam => (this.webcam = webcam)}
+        // screenshotFormat="image/jpeg"
+        audio={false}
+        videoOption={videoOption}
+      />
+
+    </div>
+  );
 }
 
 export default App
